43b96cd0157eecddb4fb545bc9e86a71a09eafd0
Leonardo <85722131+Master-Leodin@users.noreply.github.com>
2025-07-06 14:15:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//First type of funktion
+function checkPerson(name, age, openToWork){
+    return {name, age, openToWork: openToWork ? "Yes" : "No"
+}};
+const person = checkPerson("Leon", 43, true);
+console.log(`The person "${person.name}" have ${person.age} years.\nIs open to work? ${person.openToWork}`);
+
+//Second type of function
+const projects = function(link){
+    return link;
+};
+const gitHub = projects("https://github.com/Master-Leodin");
+console.log(`You can see his projects at: ${gitHub}`);
+
+//Third type of function
+const job = (typeOfJob) => typeOfJob;
+console.log(job("Looking for a sponsored visa"));
+
